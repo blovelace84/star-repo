ca5bdb0bd336bbf1ad3849d80e691d1e31ec62e4
blovelace84 <blovelace84@gmail.com>
2025-02-08 08:26:26 -0500

    Update App.jsx
    I add a title to the brower tab

--- star-destroyer/src/App.jsx
+++ star-destroyer/src/App.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Space from "./components/Space";
 import './App.css';
 import TitleScreen from "./components/TitleScreen";
 
 const App = () => {
   const [hasStarted, setHasStarted] = useState(false);
+  useEffect(() => {
+    document.title = hasStarted ? 'Star Destroyer!!' : 'Shoot the Stars!!';
+  }, [hasStarted]);
   return(
     <div className="app">
       {hasStarted ? (
